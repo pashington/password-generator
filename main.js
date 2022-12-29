920d9aceff900aea7196e05ed6d2dbaa9900709e
pashington <stefanmalakhow@gmail.com>
2022-12-29 17:53:52 +0100

    Added switchers logic on click; Changed logic of generating password; Refactored code.

--- main.js
+++ main.js
@@ -1,49 +1,64 @@
-// Array for capital letters chose
-let capitalLettersPassword = ["a","b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","f","v","x","y","z", "~", "-", "=", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// No chose array
-let vanillaPassword = ["a","b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","f","v","x","y","z", "~", "-", "=", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// Nums array
-let numsPassword = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// Array with passphrase words
-let passphrasesArray = ["big", "bob", "go", "work"];
-// Empty vars
-let password="";
-let passphrase="";
-//Generate password
+const items = ["bob", "go", "big", "wrap", "work", "a","b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","f","v","x","y","z", "~", "-", "=", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 function generatePassword(signCount, source){
-    password = "";
+    let password = "";
     for(let i=0; i<signCount; i++){
         password += source[Math.floor(Math.random()*source.length)];
-
     }
     document.getElementById("password-input").value = password;
 }
 //Generate passphrase
 function generatePassphrase(separator, source){
-    passphrase = "";
+    let passphrase = "";
     for(let i=0; i<3; i++){
         passphrase += source[Math.floor(Math.random()*source.length)];
-        if(i===2){
-            // pass
-        }else{
-            passphrase += separator;
+        if(i<2){
+            passphrase+=separator;
         }
     }
     document.getElementById("password-input").value = passphrase;
 }
 
-document.getElementById("generate-btn").onclick = function(){
-    if(document.getElementById("capital-letters-checkbox").checked){
-        generatePassword(parseInt(document.getElementById("nums-input").value), capitalLettersPassword);
-    }else
-    if(document.getElementById("only-numbers-checkbox").checked){
-        generatePassword(parseInt(document.getElementById("nums-input").value), numsPassword);
-    }else
-    if(document.getElementById("passphrase-checkbox").checked){
-        generatePassphrase(document.getElementById("separator-word").value, passphrasesArray);
+const generateButton = document.getElementById("generate-btn");
+const capitalLettersCheckbox = document.getElementById("capital-letters-checkbox");
+const onlyNumsCheckbox = document.getElementById("only-numbers-checkbox");
+const passphraseCheckbox = document.getElementById("passphrase-checkbox");
+const separatorInput = document.getElementById("separator-word");
+const numsInput = document.getElementById("nums-input");
+
+// Event listeners on clicks to disable other inputs
+passphraseCheckbox.addEventListener("click", function(){
+    if(passphraseCheckbox.checked){
+        capitalLettersCheckbox.checked = false;
+        onlyNumsCheckbox.checked = false;
     }
-    else{
-        generatePassword(parseInt(document.getElementById("nums-input").value), vanillaPassword);
+});
+onlyNumsCheckbox.addEventListener("click", function(){
+    if(onlyNumsCheckbox.checked){
+        passphraseCheckbox.checked = false;
+        capitalLettersCheckbox.checked = false;
+    }
+});
+capitalLettersCheckbox.addEventListener("click", function(){
+    if(capitalLettersCheckbox.checked){
+        passphraseCheckbox.checked = false;
+        onlyNumsCheckbox.checked = false;
+    }
+});
+// Generate logic
+generateButton.onclick = function(){
+    switch(true){
+        case capitalLettersCheckbox.checked:
+            generatePassword(parseInt(numsInput.value), items.slice(5));
+            break;
+        case onlyNumsCheckbox.checked:
+            generatePassword(parseInt(numsInput.value), items.slice(35, 45));
+            break;
+        case  passphraseCheckbox.checked:
+            generatePassphrase(separatorInput.value, items.slice(0,5));
+            break;
+        default:
+            generatePassword(numsInput.value, items.slice(5,40));
+            break;
     }
 }
 
